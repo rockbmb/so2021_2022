e9a7ef3c73c4d05c5934b63f9a32df4a5d76581c
Alexandre Baldé (WSL Win11 Pro) <alexandrer_b@outlook.com>
2022-02-23 22:58:57 +0000

    Add conditional compilation to different readln versions

--- TP/guiao1/ex3,4,5/mynl.c
+++ TP/guiao1/ex3,4,5/mynl.c
@@ -7,7 +7,11 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
+#ifdef SLOW
 #include "readln1.h"
+#else
+#include "readln2.h"
+#endif
 
 #define BUFSIZ 8
 
@@ -15,10 +19,20 @@ int main(int argc, char * argv[]) {
     int count = 1;
     char * line = malloc(BUFSIZ * sizeof(char));
     memset(line, BUFSIZ, 0);
+#ifdef SLOW
+    printf("BOOM!");
+#else
+    MYFILE * mf = myopen(STDOUT_FILENO, 16);
+    printf("BOW!");
+#endif
 
     ssize_t res = 0;
     read_line:
+#ifdef SLOW
     while (res = readln1(STDIN_FILENO, line, BUFSIZ)) {
+#else
+    while (res = readln2(mf, line, BUFSIZ)) {
+#endif
         if (res < 0) {
             perror("Erro a ler de STDIN");
             return 1;
@@ -43,7 +57,11 @@ int main(int argc, char * argv[]) {
             bool more = true;
             write(STDOUT_FILENO, line, res);
             while (more) {
+#ifdef SLOW
                 res = readln1(STDIN_FILENO, line, BUFSIZ);
+#else
+                res = readln2(mf, line, BUFSIZ);
+#endif
                 //printf("res: %ld\n", res);
                 if (res < 0) {
                     perror("Erro a ler de STDIN");
@@ -60,5 +78,9 @@ int main(int argc, char * argv[]) {
         memset(line, BUFSIZ, 0);
     }
 
+#ifdef SLOW
+#else
+    myclose(mf);
+#endif
     return 0;
 }
